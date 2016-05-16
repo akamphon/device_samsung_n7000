d732ccd48f0be9ba9ea5ab2edd80e077b9c0db43
sup akamphon <sup@engr.tu.ac.th>
2016-05-16 12:08:09 +0700

    Duify?
    Change-Id: Ife6531d704964f438542f0399634bd6005a9468e

--- du.mk
+++ du.mk
@@ -18,14 +18,14 @@
 PRODUCT_RELEASE_NAME := GT-N7000
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/du/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/n7000/full_n7000.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := n7000
-PRODUCT_NAME := cm_n7000
+PRODUCT_NAME := du_n7000
 PRODUCT_BRAND := Samsung
 PRODUCT_MODEL := GT-N7000
 
